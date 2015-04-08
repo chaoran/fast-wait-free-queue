348d458bccb9056b07a107d91a5d8923d9b4f816
Chaoran Yang <chaorany@me.com>
2015-04-08 00:14:19 -0500

    Remove useless code.

--- ccqueue.h
+++ ccqueue.h
@@ -10,72 +10,70 @@
 #define MAX_THREADS 512
 
 typedef struct Node {
-    Object val;
-    volatile struct Node *next;
+  Object val;
+  volatile struct Node *next;
 } Node;
 
 typedef struct QueueCCSynchStruct {
-    CCSynchStruct enqueue_struct CACHE_ALIGN;
-    CCSynchStruct dequeue_struct CACHE_ALIGN;
-    volatile Node *last CACHE_ALIGN;
-    volatile Node *first CACHE_ALIGN;
-    Node guard CACHE_ALIGN;
-    PoolStruct pool_node[MAX_THREADS] CACHE_ALIGN;
+  CCSynchStruct enqueue_struct CACHE_ALIGN;
+  CCSynchStruct dequeue_struct CACHE_ALIGN;
+  volatile Node *last CACHE_ALIGN;
+  volatile Node *first CACHE_ALIGN;
 } QueueCCSynchStruct;
 
-
 typedef struct QueueThreadState {
-    ThreadState enqueue_thread_state;
-    ThreadState dequeue_thread_state;
+  ThreadState enqueue_thread_state;
+  ThreadState dequeue_thread_state;
 } QueueThreadState;
 
-
 inline static void queueCCSynchInit(QueueCCSynchStruct *queue_object_struct) {
-    CCSynchStructInit(&queue_object_struct->enqueue_struct);
-    CCSynchStructInit(&queue_object_struct->dequeue_struct);
-    queue_object_struct->guard.val = GUARD;
-    queue_object_struct->guard.next = null;
-    queue_object_struct->first = &queue_object_struct->guard;
-    queue_object_struct->last = &queue_object_struct->guard;
+  CCSynchStructInit(&queue_object_struct->enqueue_struct);
+  CCSynchStructInit(&queue_object_struct->dequeue_struct);
+
+  Node * dummy = malloc(sizeof(Node));
+  dummy->val = GUARD;
+  dummy->next = null;
+
+  queue_object_struct->first = dummy;
+  queue_object_struct->last = dummy;
 }
 
 inline static void queueThreadStateInit(QueueCCSynchStruct *object_struct, QueueThreadState *lobject_struct, int pid) {
-    threadStateInit(&lobject_struct->enqueue_thread_state, (int)pid);
-    threadStateInit(&lobject_struct->dequeue_thread_state, (int)pid);
-    init_pool(&object_struct->pool_node[pid], sizeof(Node));
+  threadStateInit(&lobject_struct->enqueue_thread_state, (int)pid);
+  threadStateInit(&lobject_struct->dequeue_thread_state, (int)pid);
 }
 
-
 inline static RetVal serialEnqueue(void *state, ArgVal arg, int pid) {
-    QueueCCSynchStruct *st = (QueueCCSynchStruct *)state;
-    Node *node;
-    
-    node = alloc_obj(&st->pool_node[pid]);
-    node->next = null;
-    node->val = arg;
-    st->last->next = node;
-    st->last = node;
-    return -1;
+  QueueCCSynchStruct *st = (QueueCCSynchStruct *)state;
+  Node *node;
+
+  node = malloc(sizeof(Node));
+  node->next = null;
+  node->val = arg;
+  st->last->next = node;
+  st->last = node;
+  return -1;
 }
 
 inline static RetVal serialDequeue(void *state, ArgVal arg, int pid) {
-    QueueCCSynchStruct *st = (QueueCCSynchStruct *)state;
-    Node *node = (Node *)st->first;
-    
-    if (st->first->next != null){
-        st->first = st->first->next;
-        return node->next->val;
-    } else {
-        return -1;
-    }
+  QueueCCSynchStruct *st = (QueueCCSynchStruct *)state;
+  Node *node = (Node *)st->first;
+
+  if (st->first->next != null){
+    st->first = st->first->next;
+    free(node);
+    return st->first->val;
+  } else {
+    return -1;
+  }
 }
 
 
 inline static void applyEnqueue(QueueCCSynchStruct *object_struct, QueueThreadState *lobject_struct, ArgVal arg, int pid) {
-     applyOp(&object_struct->enqueue_struct, &lobject_struct->enqueue_thread_state, serialEnqueue, object_struct, arg, pid);
+  applyOp(&object_struct->enqueue_struct, &lobject_struct->enqueue_thread_state, serialEnqueue, object_struct, arg, pid);
 }
 
 inline static RetVal applyDequeue(QueueCCSynchStruct *object_struct, QueueThreadState *lobject_struct, int pid) {
-     return applyOp(&object_struct->dequeue_struct, &lobject_struct->dequeue_thread_state, serialDequeue, object_struct, (ArgVal) pid, pid);
+  return applyOp(&object_struct->dequeue_struct, &lobject_struct->dequeue_thread_state, serialDequeue, object_struct, (ArgVal) pid, pid);
 }
 #endif
--- ccsynch.h
+++ ccsynch.h
@@ -1,138 +1,81 @@
 #ifndef _CCSYNCH_H_
 #define _CCSYNCH_H_
 
-#if defined(sun) || defined(_sun)
-#    include <schedctl.h>
-#endif
-
 #include "config.h"
 #include "primitives.h"
 
 const int CCSYNCH_HELP_BOUND = 256;
 
-typedef struct HalfCCSynchNode {
-    struct HalfCCSynchNode *next;
-    ArgVal arg_ret;
-    int32_t pid;
-    int32_t locked;
-    int32_t completed;
-} HalfCCSynchNode;
-
 typedef struct CCSynchNode {
-    struct CCSynchNode *next;
-    ArgVal arg_ret;
-    int32_t pid;
-    int32_t locked;
-    int32_t completed;
-    int32_t align[PAD_CACHE(sizeof(HalfCCSynchNode))];
+  struct CCSynchNode *next;
+  ArgVal arg_ret;
+  int32_t pid;
+  int32_t locked;
+  int32_t completed;
 } CCSynchNode;
 
 typedef struct ThreadState {
-    CCSynchNode *next_node;
-    int toggle;
-#if defined(__sun) || defined(sun)
-    schedctl_t *schedule_control;    
-#endif
+  CCSynchNode *next_node;
 } ThreadState;
 
 typedef struct CCSynchStruct {
-    volatile CCSynchNode *Tail CACHE_ALIGN;
-#ifdef DEBUG
-    volatile int rounds CACHE_ALIGN;
-    volatile int counter;
-    volatile int combiner_counter[N_THREADS];
-#endif
+  volatile CCSynchNode *Tail CACHE_ALIGN;
 } CCSynchStruct;
 
 
 inline static void threadStateInit(ThreadState *st_thread, int pid) {
-    st_thread->next_node = getAlignedMemory(CACHE_LINE_SIZE, sizeof(CCSynchNode));
-#ifdef sun
-    st_thread->schedule_control = schedctl_init();
-#endif
+  st_thread->next_node = getAlignedMemory(CACHE_LINE_SIZE, sizeof(CCSynchNode));
 }
 
 inline static RetVal applyOp(CCSynchStruct *l, ThreadState *st_thread, RetVal (*sfunc)(void *, ArgVal, int), void *state, ArgVal arg, int pid) {
-    volatile CCSynchNode *p;
-    volatile CCSynchNode *cur;
-    register CCSynchNode *next_node, *tmp_next;
-    register int counter = 0;
+  volatile CCSynchNode *p;
+  volatile CCSynchNode *cur;
+  register CCSynchNode *next_node, *tmp_next;
+  register int counter = 0;
 
-    next_node = st_thread->next_node;
-    next_node->next = null;
-    next_node->locked = true;
-    next_node->completed = false;
+  next_node = st_thread->next_node;
+  next_node->next = null;
+  next_node->locked = true;
+  next_node->completed = false;
 
-#if defined(__sun) || defined(sun)
-    schedctl_start(st_thread->schedule_control);
-#endif
-    cur = (CCSynchNode *)SWAP(&l->Tail, next_node);
-    cur->arg_ret = arg;
-    cur->next = (CCSynchNode *)next_node;
-    st_thread->next_node = (CCSynchNode *)cur;
-#if defined(__sun) || defined(sun)
-    schedctl_stop(st_thread->schedule_control);
-#endif
+  cur = (CCSynchNode *)SWAP(&l->Tail, next_node);
+  cur->arg_ret = arg;
+  cur->next = (CCSynchNode *)next_node;
+  st_thread->next_node = (CCSynchNode *)cur;
 
-    while (cur->locked) {                   // spinning
-#if N_THREADS > USE_CPUS
-        sched_yield();
-#elif defined(sparc)
-        Pause();
-        Pause();
-        Pause();
-        Pause();
-#else
-        Pause();
-#endif
-    }
-    if (cur->completed)                     // I have been helped
-        return cur->arg_ret;
-#if defined(__sun) || defined(sun)
-    schedctl_start(st_thread->schedule_control);
-#endif
-#ifdef DEBUG
-    l->rounds++;
-    l->combiner_counter[pid] += 1;
-#endif
-    p = cur;                                // I am not been helped
-    while (p->next != null && counter < CCSYNCH_HELP_BOUND) {
-        ReadPrefetch(p->next);
-        counter++;
-#ifdef DEBUG
-        l->counter++;
-#endif
-        tmp_next = p->next;
-        p->arg_ret = sfunc(state, p->arg_ret, p->pid);
-        p->completed = true;
-        p->locked = false;
-        WeakFence();
-        p = tmp_next;
-    }
-    p->locked = false;                      // Unlock the next one
-    StoreFence();
-#if defined(__sun) || defined(sun)
-    schedctl_stop(st_thread->schedule_control);
-#endif
+  while (cur->locked) {                   // spinning
+    Pause();
+  }
 
-    return cur->arg_ret;
+  if (cur->completed) {                   // I have been helped
+    return cur->arg_ret;;
+  }
+
+  p = cur;                                // I am not been helped
+  while (p->next != null && counter < CCSYNCH_HELP_BOUND) {
+    ReadPrefetch(p->next);
+    counter++;
+    tmp_next = p->next;
+    p->arg_ret = sfunc(state, p->arg_ret, p->pid);
+    p->completed = true;
+    p->locked = false;
+    WeakFence();
+    p = tmp_next;
+  }
+
+  p->locked = false;                      // Unlock the next one
+  StoreFence();
+
+  return cur->arg_ret;
 }
 
 void CCSynchStructInit(CCSynchStruct *l) {
-#ifdef DEBUG
-    int i;
+  l->Tail = getAlignedMemory(CACHE_LINE_SIZE, sizeof(CCSynchNode));
+  l->Tail->next = null;
+  l->Tail->locked = false;
+  l->Tail->completed = false;
 
-    l->rounds = l->counter =0;
-    for (i=0; i < N_THREADS; i++)
-        l->combiner_counter[i] += 1;
-#endif
-
-    l->Tail = getAlignedMemory(CACHE_LINE_SIZE, sizeof(CCSynchNode));
-    l->Tail->next = null;
-    l->Tail->locked = false;
-    l->Tail->completed = false;
-
-    StoreFence();
+  StoreFence();
 }
 
 #endif
