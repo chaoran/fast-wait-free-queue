eca98ecfe35575cca74ffcf169681fca8960db74
Chaoran Yang <chaorany@me.com>
2015-04-14 20:29:01 -0500

    Remove useless code.

--- fifo.h
+++ fifo.h
@@ -4,15 +4,14 @@
 #include "align.h"
 
 struct _fifo_node_t;
-struct _fifo_nodepair_t {
-  size_t index;
-  struct _fifo_node_t * node;
-};
 
 typedef struct DOUBLE_CACHE_ALIGNED {
   size_t enq DOUBLE_CACHE_ALIGNED;
   size_t deq DOUBLE_CACHE_ALIGNED;
-  volatile struct _fifo_nodepair_t head DOUBLE_CACHE_ALIGNED;
+  volatile struct {
+    int index;
+    struct _fifo_node_t * node;
+  } head DOUBLE_CACHE_ALIGNED;
   size_t size;
   size_t nprocs;
 } fifo_t;
@@ -27,10 +26,7 @@ typedef struct DOUBLE_CACHE_ALIGNED _fifo_handle_t {
 
 void fifo_init(fifo_t * fifo, size_t size, size_t width);
 void fifo_register(fifo_t * fifo, fifo_handle_t * handle);
-void fifo_unregister(fifo_t * fifo, fifo_handle_t * handle);
 void * fifo_get(fifo_t * fifo, fifo_handle_t * handle);
 void fifo_put(fifo_t * fifo, fifo_handle_t * handle, void * data);
-void fifo_aget(fifo_t * fifo, fifo_handle_t * handle);
-void * fifo_test(fifo_t * fifo, fifo_handle_t * handle);
 
 #endif /* end of include guard: FIFO_H */
