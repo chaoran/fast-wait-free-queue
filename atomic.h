e0b5e7c4318bab791596b152cc5e11834c1608d1
Chaoran Yang <chaorany@me.com>
2015-09-15 14:27:10 -0500

    Adjust btas definition to use macros.
    So it will compile with -O0.

--- atomic.h
+++ atomic.h
@@ -134,21 +134,13 @@ int _atomic_dcas(volatile long * ptr, long * cmp1, long * cmp2,
   _atomic_dcas((volatile long *) p, \
       (long *) o1, (long *) o2, (long) n1, (long) n2)
 
-static inline
-int _atomic_btas(volatile long * ptr, char bit)
-{
-  char success;
-
-  __asm__ __volatile__(
-      "lock btsq %2, %0\n"
-      "setnc %1"
-      : "+m" (*ptr), "=r" (success)
-      : "ri" (bit)
-      : "cc" );
-
-  return success;
-}
-#define atomic_btas(ptr, bit) _atomic_btas((volatile long *) ptr, bit)
+#define atomic_btas(ptr, bit) ({ \
+  char __ret; \
+  __asm__ __volatile__( \
+      "lock btsq %2, %0; setnc %1" \
+      : "+m" (*ptr), "=r" (__ret) : "ri" (bit) : "cc" ); \
+  __ret; \
+})
 
 #else
 #define spin_while(cond) while (cond) __asm__("nop")
