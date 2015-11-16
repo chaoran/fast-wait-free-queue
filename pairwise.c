2c71052abed87ec6a903da6c4f6a8025c4941358
Chaoran Yang <chaorany@me.com>
2015-11-15 21:12:15 -0600

    Use aligned memory for queue and handles.

--- pairwise.c
+++ pairwise.c
@@ -5,8 +5,8 @@
 #include "queue.h"
 
 static long nops;
-static queue_t q;
-static handle_t * hds;
+static queue_t * q;
+static handle_t ** hds;
 
 void init(int nprocs, int logn) {
 
@@ -22,27 +22,30 @@ void init(int nprocs, int logn) {
 
   printf("  Number of operations: %d\n", nops);
 
-  hds = malloc(sizeof(handle_t [nprocs]));
-  queue_init(&q, nprocs);
+  q = aligned_alloc(PAGE_SIZE, sizeof(queue_t));
+  queue_init(q, nprocs);
+
+  hds = aligned_alloc(PAGE_SIZE, sizeof(handle_t * [nprocs]));
 }
 
 void thread_init(int id, int nprocs) {
-  queue_register(&q, &hds[id]);
+  hds[id] = aligned_alloc(PAGE_SIZE, sizeof(handle_t));
+  queue_register(q, hds[id]);
 }
 
 void * benchmark(int id, int nprocs) {
   void * val = (void *) (intptr_t) (id + 1);
-  handle_t * th = &hds[id];
+  handle_t * th = hds[id];
 
   delay_t state;
   delay_init(&state, id);
 
   int i;
   for (i = 0; i < nops / nprocs; ++i) {
-    enqueue(&q, th, val);
+    enqueue(q, th, val);
     delay_exec(&state);
 
-    dequeue(&q, th);
+    dequeue(q, th);
     delay_exec(&state);
   }
 
