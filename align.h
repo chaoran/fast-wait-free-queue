e6436d645080186915787fd3a77d759254d4ecea
Chaoran Yang <chaorany@me.com>
2015-11-16 00:43:08 -0600

    Add impl for aligned_alloc.

--- align.h
+++ align.h
@@ -6,4 +6,13 @@
 #define CACHE_ALIGNED __attribute__((aligned(CACHE_LINE_SIZE)))
 #define DOUBLE_CACHE_ALIGNED __attribute__((aligned(2 * CACHE_LINE_SIZE)))
 
+#include <stddef.h>
+
+static inline void * aligned_alloc(size_t align, size_t size)
+{
+  void * ptr;
+  posix_memalign(&ptr, align, size);
+  return ptr;
+}
+
 #endif /* end of include guard: ALIGN_H */
