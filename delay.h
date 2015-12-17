1dec7cc40215a7f692987749e15c05062fe11d4f
Chaoran Yang <chaorany@me.com>
2015-12-17 00:55:14 -0600

    Use time based seed for generating random numbers.

--- delay.h
+++ delay.h
@@ -1,13 +1,14 @@
 #ifndef DELAY_H
 #define DELAY_H
 
+#include <time.h>
 #include <stdlib.h>
 
 typedef struct drand48_data delay_t;
 
 static inline void delay_init(delay_t * state, int id)
 {
-  srand48_r(id + 1, state);
+  srand48_r(time(NULL) + id, state);
 }
 
 static inline void delay_exec(delay_t * state)
