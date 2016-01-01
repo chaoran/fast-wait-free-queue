54549c83730be09869b02b2c04834606aef36006
Chaoran Yang <chaorany@me.com>
2015-12-31 21:35:41 -0600

    Fix a bug.

--- cpumap.h
+++ cpumap.h
@@ -9,7 +9,7 @@ int cpumap(int i, int nprocs)
   return (i / 36) * 36 + (i % 2) * 18 + (i % 36 / 2);
 }
 
-#ifdef GUADALUPE_OVERSUB
+#elif GUADALUPE_OVERSUB
 int cpumap(int i, int nprocs) {
   return (i % 18);
 }
